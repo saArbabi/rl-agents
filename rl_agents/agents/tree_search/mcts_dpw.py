2266f6d2a59a7a95b3f8d9643403d0a246e2b36e
Salar <salar.arbabi.i@gmail.com>
2020-07-26 13:22:53 +0430

    added fake chance node

--- rl_agents/agents/tree_search/mcts_dpw.py
+++ rl_agents/agents/tree_search/mcts_dpw.py
@@ -136,19 +136,26 @@ class MCTS(AbstractPlanner):
         :param state: the initial environment state
         :param observation: the corresponding observation
         """
-        node = self.root
+        decision_node = self.root
         total_reward = 0
         depth = 0
         terminal = False
         state.seed(self.np_random.randint(2**30))
-        while depth < self.config['horizon'] and node.children and not terminal:
-            action = node.sampling_rule(temperature=self.config['temperature'])
+        while depth < self.config['horizon']/2 and decision_node.children and not terminal:
+            action = decision_node.sampling_rule(temperature=self.config['temperature'])
+
+            # perform transition
             observation, reward, terminal, _ = self.step(state, action)
-            total_reward += self.config["gamma"] ** depth * reward
             node_observation = observation if self.config["closed_loop"] else None
-            node = node.get_child(action, observation=node_observation)
+            # decision_node = chance_node.get_child(node_observation)
+            chance_node = decision_node.get_child(action, observation=node_observation)
+            decision_node = decision_node.get_child(action, observation=node_observation)
+
+            total_reward += self.config["gamma"] ** depth * reward
             depth += 1
-            # print(node.depth)
+            print('decision value ',decision_node.value)
+            print('chance value ',chance_node.value)
+
             # print(type(self)(depth))
 
             """
@@ -161,15 +168,14 @@ class MCTS(AbstractPlanner):
                 print(self.np_random.choice(list(node.children.keys())))
             """
 
-        if not node.children \
+        if not decision_node.children \
                 and depth < self.config['horizon'] \
-                and (not terminal or node == self.root):
-            node.expand(self.prior_policy(state, observation))
+                and (not terminal or decision_node == self.root):
+            decision_node.expand(self.prior_policy(state, observation))
 
         if not terminal:
             total_reward = self.evaluate(state, observation, total_reward, depth=depth)
-        print('update set now')
-        node.update_branch(total_reward)
+        decision_node.update_branch(total_reward)
 
     def evaluate(self, state, observation, total_reward=0, depth=0):
         """
@@ -287,6 +293,110 @@ class DecisionNode(Node):
         if observation is not None:
             if str(observation) not in child.children:
                 child.children[str(observation)] = DecisionNode(parent=child, planner=self.planner, prior=0)
+            child = child.children[str(observation)]
+        return child
+
+    def selection_strategy(self, temperature):
+        """
+            Select an action according to its value, prior probability and visit count.
+
+        :param temperature: the exploration parameter, positive or zero.
+        :return: the selected action with maximum value and exploration bonus.
+        """
+        if not self.parent:
+            return self.get_value()
+
+        # return self.value + temperature * self.prior * np.sqrt(np.log(self.parent.count) / self.count)
+        return self.get_value() + temperature * len(self.parent.children) * self.prior/(self.count+1)
+
+    def convert_visits_to_prior_in_branch(self, regularization=0.5):
+        """
+            For any node in the subtree, convert the distribution of all children visit counts to prior
+            probabilities, and reset the visit counts.
+
+        :param regularization: in [0, 1], used to add some probability mass to all children.
+                               when 0, the prior is a Boltzmann distribution of visit counts
+                               when 1, the prior is a uniform distribution
+        """
+        self.count = 0
+        total_count = sum([(child.count+1) for child in self.children.values()])
+        for child in self.children.values():
+            child.prior = regularization*(child.count+1)/total_count + regularization/len(self.children)
+            child.convert_visits_to_prior_in_branch()
+
+    def get_value(self):
+        return self.value
+
+class ChanceNode(Node):
+    K = 1.0
+    """ The value function first-order filter gain"""
+
+    def __init__(self, parent, planner, prior=1):
+        super(ChanceNode, self).__init__(parent, planner)
+        self.value = 0
+        self.prior = prior
+        self.depth = parent.depth
+
+    def selection_rule(self):
+        if not self.children:
+            return None
+        # Tie best counts by best value
+        actions = list(self.children.keys())
+        counts = Node.all_argmax([self.children[a].count for a in actions])
+        return actions[max(counts, key=(lambda i: self.children[actions[i]].get_value()))]
+
+    def sampling_rule(self, temperature=None):
+        """
+            Select an action from the node.
+            - if exploration is wanted with some temperature, follow the selection strategy.
+            - else, select the action with maximum visit count
+
+        :param temperature: the exploration parameter, positive or zero
+        :return: the selected action
+        """
+        if self.children:
+            actions = list(self.children.keys())
+            # Randomly tie best candidates with respect to selection strategy
+            indexes = [self.children[a].selection_strategy(temperature) for a in actions]
+            return actions[self.random_argmax(indexes)]
+        else:
+            return None
+
+    def expand(self, actions_distribution):
+        """
+            Expand a leaf node by creating a new child for each available action.
+
+        :param actions_distribution: the list of available actions and their prior probabilities
+        """
+        actions, probabilities = actions_distribution
+        for i in range(len(actions)):
+            if actions[i] not in self.children:
+                self.children[actions[i]] = type(self)(self, self.planner, probabilities[i])
+
+    def update(self, total_reward):
+        """
+            Update the visit count and value of this node, given a sample of total reward.
+
+        :param total_reward: the total reward obtained through a trajectory passing by this node
+        """
+        self.count += 1
+        self.value += self.K / self.count * (total_reward - self.value)
+
+    def update_branch(self, total_reward):
+        """
+            Update the whole branch from this node to the root with the total reward of the corresponding trajectory.
+
+        :param total_reward: the total reward obtained through a trajectory passing by this node
+        """
+        self.update(total_reward)
+        if self.parent:
+            self.parent.update_branch(total_reward)
+
+    def get_child(self, action, observation=None):
+        child = self.children[action]
+        if observation is not None:
+            if str(observation) not in child.children:
+                child.children[str(observation)] = ChanceNode(parent=child, planner=self.planner, prior=0)
             child = child.children[str(observation)]
         return child
 
