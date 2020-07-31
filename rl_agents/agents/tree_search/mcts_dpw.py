fd0d9a2cac84b4ad1acb2c86c5dc5024a67b5def
Salar <salar.arbabi.i@gmail.com>
2020-07-31 19:48:46 +0430

    added action pw, but breaking atm

--- rl_agents/agents/tree_search/mcts_dpw.py
+++ rl_agents/agents/tree_search/mcts_dpw.py
@@ -10,17 +10,14 @@ logger = logging.getLogger(__name__)
 """
 see [Adrien Coutoux, 2011][Continuous Upper Confidence Trees]
 """
-
-
 class MCTSDPWAgent(AbstractTreeSearchAgent):
     """
         An MCTS_DPW agent that uses Double Progressive Widenning for handling continuous
         stochastic MDPs.
     """
     def make_planner(self):
-        prior_policy = MCTSDPWAgent.policy_factory(self.config["prior_policy"])
         rollout_policy = MCTSDPWAgent.policy_factory(self.config["rollout_policy"])
-        return MCTSDPW(self.env, prior_policy, rollout_policy, self.config)
+        return MCTSDPW(self.env,rollout_policy, self.config)
 
     @classmethod
     def default_config(cls):
@@ -28,7 +25,6 @@ class MCTSDPWAgent(AbstractTreeSearchAgent):
         config.update({
             "budget": 100,
             "horizon": None,
-            "prior_policy": {"type": "random_available"},
             "rollout_policy": {"type": "random_available"},
             "env_preprocessors": []
          })
@@ -106,17 +102,15 @@ class MCTSDPW(AbstractPlanner):
        An implementation of Monte-Carlo Tree Search, with Upper Confidence Tree exploration
        and Double Progressive Widenning.
     """
-    def __init__(self, env, prior_policy, rollout_policy, config=None):
+    def __init__(self, env, rollout_policy, config=None):
         """
             New MCTSDPW instance.
 
         :param config: the mcts configuration. Use default if None.
-        :param prior_policy: the prior policy used when expanding and selecting nodes
         :param rollout_policy: the rollout policy used to estimate the value of a leaf node
         """
         super().__init__(config)
         self.env = env
-        self.prior_policy = prior_policy
         self.rollout_policy = rollout_policy
         if not self.config["horizon"]:
             self.config["episodes"], self.config["horizon"] = \
@@ -163,29 +157,31 @@ class MCTSDPW(AbstractPlanner):
         depth = 0
         terminal = False
         state.seed(self.np_random.randint(2**30))
+        # print('haha:', len(decision_node.children))
 
-        while depth < self.config['horizon'] and decision_node.children \
-                                            and not terminal:
-            action = decision_node.sampling_rule(temperature=self.config['temperature'])
+        while depth < self.config['horizon'] and decision_node.count != 0 and not terminal:
 
             # perform an action followed by a transition
-            chance_node = decision_node.get_child(action)
+            chance_node, action = decision_node.get_child(state, temperature=self.config['temperature'])
+            # print('after:', len(chance_node.children))
+
             observation, reward, terminal, _ = self.step(state, action)
             node_observation = observation if self.config["closed_loop"] else None
-
+            # print('before:', chance_node.children)
             decision_node = chance_node.get_child(node_observation)
+            # print('after:', len(chance_node.children))
+
+            # print('after:', len(chance_node.children))
 
             total_reward += self.config["gamma"] ** depth * reward
             depth += 1
-            print(depth)
-
-        if not decision_node.children \
-                and depth < self.config['horizon'] \
-                and (not terminal or decision_node == self.root):
-            decision_node.expand(state, self.prior_policy(state, observation))
-
+            # print(depth)
+            print(decision_node.depth)
+            # print(chance_node.value)
+        # print('break')
         if not terminal:
             total_reward = self.evaluate(state, observation, total_reward, depth=depth)
+
         # Backup global statistics
         decision_node.backup_to_root(total_reward)
 
@@ -220,33 +216,18 @@ class MCTSDPW(AbstractPlanner):
         return self.get_plan()
 
     def step_planner(self, action):
-        if self.config["step_strategy"] == "prior":
-            self.step_by_prior(action)
-        else:
-            super().step_planner(action)
-
-    def step_by_prior(self, action):
-        """
-            Replace the MCTS tree by its subtree corresponding to the chosen action, but also convert the visit counts
-            to prior probabilities and before resetting them.
-
-        :param action: a chosen action from the root node
-        """
-        self.step_by_subtree(action)
-        self.root.convert_visits_to_prior_in_branch()
-
+        super().step_planner(action)
 
 class DecisionNode(Node):
-    # state progressive widenning parameters
+    # action progressive widenning parameters
     k_action = 4
-    alpha_action = 0.2
+    alpha_action = 0.1
     K = 1.0
     """ The value function first-order filter gain"""
 
-    def __init__(self, parent, planner, prior=1):
+    def __init__(self, parent, planner):
         super(DecisionNode, self).__init__(parent, planner)
         self.value = 0
-        self.prior = prior
         self.depth = 0 if parent is None else parent.depth + 1
 
     def selection_rule(self):
@@ -257,36 +238,29 @@ class DecisionNode(Node):
         counts = Node.all_argmax([self.children[a].count for a in actions])
         return actions[max(counts, key=(lambda i: self.children[actions[i]].get_value()))]
 
-    def sampling_rule(self, temperature=None):
-        """
-            Select an action from the node.
-            - if exploration is wanted with some temperature, follow the selection strategy.
-            - else, select the action with maximum visit count
-
-        :param temperature: the exploration parameter, positive or zero
-        :return: the selected action
-        """
-        if self.children:
-            actions = list(self.children.keys())
-            # Randomly tie best candidates with respect to selection strategy
-            indexes = [self.children[a].selection_strategy(temperature) for a in actions]
-            return actions[self.random_argmax(indexes)]
-        else:
-            return None
-
-    def get_child(self, action):
-        return self.children[action]
-
-    def expand(self, state, actions_distribution):
+    def unexplored_actions(self, state):
+        if state is None:
+            raise Exception("The state should be set before expanding a node")
         try:
-            actions, probabilities = actions_distribution
+            actions = state.get_available_actions()
         except AttributeError:
             actions = range(state.action_space.n)
-        for i in range(len(actions)):
+        return set(self.children.keys()).symmetric_difference(set(actions))
 
-            self.children[actions[i]] = (self, self.planner, probabilities[i])
-            self.children[actions[i]] = ChanceNode(self, self.planner)
+    def insert_action_node(self, state):
+        action = self.planner.np_random.choice(list(self.unexplored_actions(state)))
+        self.children[action] = ChanceNode(self, self.planner)
+        return self.children[action], action
 
+    def get_child(self, state, temperature=None):
+        # print(len(self.children))
+        if len(self.children) == len(state.get_available_actions()) \
+                or self.k_action*self.count**self.alpha_action < len(self.children):
+            # select one of previously expanded actions
+            return self.selection_strategy(temperature)
+        else:
+            # insert a new aciton
+            return self.insert_action_node(state)
 
     def update(self, total_reward):
         """
@@ -309,32 +283,21 @@ class DecisionNode(Node):
 
     def selection_strategy(self, temperature):
         """
-            Select an action according to its value, prior probability and visit count.
+            Select an action according to UCB.
 
         :param temperature: the exploration parameter, positive or zero.
         :return: the selected action with maximum value and exploration bonus.
         """
-        if not self.parent:
-            return self.get_value()
+        actions = list(self.children.keys())
+        indexes = []
+        for a in actions:
+            ucb_val = self.get_value() + temperature * np.sqrt(np.log(self.count / self.children[a].count))
+            indexes.append(ucb_val)
 
-        # return self.value + temperature * self.prior * np.sqrt(np.log(self.parent.count) / self.count)
-        # return self.get_value() + temperature * np.sqrt(np.log(self.parent.count) / (self.count+1))
-        return self.get_value() + temperature * len(self.parent.children) * self.prior/(self.count+1)
-
-    def convert_visits_to_prior_in_branch(self, regularization=0.5):
-        """
-            For any node in the subtree, convert the distribution of all children visit counts to prior
-            probabilities, and reset the visit counts.
-
-        :param regularization: in [0, 1], used to add some probability mass to all children.
-                               when 0, the prior is a Boltzmann distribution of visit counts
-                               when 1, the prior is a uniform distribution
-        """
-        self.count = 0
-        total_count = sum([(child.count+1) for child in self.children.values()])
-        for child in self.children.values():
-            child.prior = regularization*(child.count+1)/total_count + regularization/len(self.children)
-            child.convert_visits_to_prior_in_branch()
+            # TODO: Some paper return most tried aciton
+        # print(indexes)
+        action = actions[self.random_argmax(indexes)]
+        return self.children[action], action
 
     def get_value(self):
         return self.value
@@ -345,24 +308,26 @@ class ChanceNode(Node):
     k_state = 4
     alpha_state = 0.2
 
-    def __init__(self, parent, planner, prior=1):
+    def __init__(self, parent, planner):
         assert parent is not None
         super(ChanceNode, self).__init__(parent, planner)
         self.value = 0
         self.depth = parent.depth
 
     def insert_state_node(self, observation):
-        # Generate placeholder node
         self.children[str(observation)] = DecisionNode(self, self.planner)
 
-    def get_child(self, observation, hash=False):
-        if not self.children:
-            self.insert_state_node(observation)
+    def get_child(self, observation):
         if str(observation) not in self.children:
+            print(len(self.children), 'state childdd')
 
-            if self.k_state*self.count**self.alpha_state <= len(self.children):
+            # if self.k_state*self.count**self.alpha_state < len(self.children):
+            if len(self.children)>0:
+
+                print(len(self.children), 'state childdd')
+
                 # Randomly select a previously generated child
-                random_state = self.planner.np_random.choice(list(self.children.keys()))
+                random_state = self.planner.np_random.choice(self.children)
                 return self.children[random_state]
             else:
                 # Add observation to the children set
@@ -389,22 +354,6 @@ class ChanceNode(Node):
         assert self.parent
         self.update(total_reward)
         self.parent.backup_to_root(total_reward)
-
-    def selection_strategy(self, temperature):
-        """
-            Select an action according to its value, prior probability and visit count.
-
-        :param temperature: the exploration parameter, positive or zero.
-        :return: the selected action with maximum value and exploration bonus.
-        """
-        if not self.parent:
-            return self.get_value()
-        # print('self.parent.count',self.parent.count)
-        # print('self.count',self.count)
-        # print('self.get_value()',self.get_value())
-
-        # return self.value + temperature * self.prior * np.sqrt(np.log(self.parent.count) / self.count)
-        return self.get_value() + temperature * np.sqrt(np.log(self.parent.count) / (self.count+1))
 
     def get_value(self):
         return self.value
